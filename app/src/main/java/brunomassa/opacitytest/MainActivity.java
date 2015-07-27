d48c86a6a76c9a3f2fc8acf609652d604fbd5ae4
Bruno Massa <massa_ferreira@hotmail.com>
2015-07-27 14:04:12 +0100

    Initial commit

---
+++ app/src/main/java/brunomassa/opacitytest/MainActivity.java
@@ -0,0 +1,51 @@
+package brunomassa.opacitytest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout relativeLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        relativeLayout=(RelativeLayout)findViewById(R.id.rl);
+
+        AlphaAnimation alpha = new AlphaAnimation(0.5F, 0.5F);
+        alpha.setDuration(0); // Poem a animação instantanea
+        alpha.setFillAfter(true); // Mantem a animação depois do fim
+
+        relativeLayout.startAnimation(alpha);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
